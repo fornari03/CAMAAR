d3e8eee545cc6442e84303824099937a24307bec
fornari03 <221017023@aluno.unb.br>
2025-11-15 19:14:45 -0300

    spec: adiciona cenários da feature de o usuário definir a senha

---
+++ src/features/step_definitions/definir_senha_usuario_steps.rb
@@ -0,0 +1,35 @@
+Dado('que o usuário {string} foi importado e está com o status {string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('um link de definição de senha válido foi enviado para {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('eu acesso a página {string} usando o link válido') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('eu devo ser redirecionado para a página de {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('o status do usuário {string} no sistema deve ser {string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('eu devo permanecer na página {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('eu devo ver a mensagem de erro {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado('que o usuário {string} já está ativo no sistema') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('eu acesso a página {string} usando o link antigo') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
