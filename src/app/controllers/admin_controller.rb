8f673acbc10bc05f0eeb44f544664ecbf0c1a99e
fornari03 <221017023@aluno.unb.br>
2025-12-06 12:33:13 -0300

    feat: implementa importar_dados() do AdminController

--- src/app/controllers/admin_controller.rb
+++ src/app/controllers/admin_controller.rb
@@ -1,4 +1,15 @@
 class AdminController < ApplicationController
   def importar_dados
+    puts "🟢 CHEGOU NO CONTROLLER!"
+    begin
+      SigaaImporter.call
+      puts "🟢 SERVICE EXECUTADO SEM ERROS!"
+      flash[:notice] = "Dados importados com sucesso!"
+    rescue StandardError => e
+      puts "🔴 ERRO NO CONTROLLER: #{e.message}"
+      puts e.backtrace.first(5)
+      flash[:alert] = e.message
+    end
+    redirect_back(fallback_location: "/gerenciamento") 
   end
 end
