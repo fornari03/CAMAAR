dd70ece63adf586eb332028819819c0b7783741d
fornari03 <221017023@aluno.unb.br>
2025-12-06 17:15:48 -0300

    refac: implementa método 'path_to' em 'common_steps.rb' pra traduzir nomes pra rotas

--- src/features/step_definitions/common_steps.rb
+++ src/features/step_definitions/common_steps.rb
@@ -18,8 +18,22 @@ Dado('que eu estou logado como Administrador') do
   # click_button 'Entrar'
 end
 
-Dado('estou na página {string}') do |string|
-  visit "/" + string
+Dado('estou na página {string}') do |nome_da_pagina|
+  visit path_to(nome_da_pagina)
+end
+
+# método auxiliar para traduzir nomes para rotas
+def path_to(page_name)
+  case page_name
+  when "gerenciamento"
+    admin_gerenciamento_path 
+    
+  when "home", "inicial"
+    root_path
+    
+  else
+    raise "Não sei o caminho para a página '#{page_name}'. Adicione no step definition."
+  end
 end
 
 Então('eu devo ver a mensagem de erro {string}') do |mensagem|
