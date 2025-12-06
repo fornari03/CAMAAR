3d0e10dd9c9d9f9a33db110bc9578ca4ff722f54
fornari03 <221017023@aluno.unb.br>
2025-12-06 19:55:07 -0300

    spec: adiciona novos testes no 'sigaa_importer_spec.rb', para atualização de dados

--- src/spec/services/sigaa_importer_spec.rb
+++ src/spec/services/sigaa_importer_spec.rb
@@ -89,6 +89,126 @@ RSpec.describe SigaaImporter do
       end
     end
 
+    context 'quando os dados já existem mas mudaram no SIGAA' do
+      let(:classes_json) do
+        [
+          {
+            "name" => "BANCOS DE DADOS AVANÇADO",
+            "code" => "CIC0097",
+            "class" => { "semester" => "2024.1", "time" => "35T23" }
+          }
+        ].to_json
+      end
+
+      let(:members_json) do
+        [
+          {
+            "code" => "CIC0097",
+            "classCode" => "TA",
+            "semester" => "2024.1",
+            "dicente" => [
+              {
+                "nome" => "Fulano da Silva", 
+                "matricula" => "150084006",
+                "usuario" => "150084006",
+                "email" => "fulano.novo@gmail.com",
+                "ocupacao" => "dicente"
+              }
+            ],
+            "docente" => {
+              "nome" => "Prof. Real", "usuario" => "88888", "email" => "prof@real.com", "ocupacao" => "docente"
+            }
+          }
+        ].to_json
+      end
+
+      let!(:aluno_existente) do
+        Usuario.create!(
+          nome: "Fulano de Tal",
+          matricula: "150084006",
+          usuario: "150084006",
+          email: "fulano.antigo@email.com",
+          password: "123", ocupacao: :discente, status: true
+        )
+      end
+
+      let!(:turma_existente) do
+        t = Turma.create!(
+          nome: "BANCOS DE DADOS",
+          codigo: "CIC0097",
+          docente: docente,
+          semestre: "2024.1", horario: "35T23", status: true
+        )
+        t.materia = Materia.create!(nome: "BD", codigo: "CIC0097")
+        t.save!
+        t
+      end
+
+      let!(:aluno_removido) do
+        Usuario.create!(
+          nome: "Beltrano", 
+          matricula: "150084008",
+          usuario: "150084008", email: "beltrano@email.com", password: "123", ocupacao: :discente, status: true
+        )
+      end
+
+      let!(:turma_removida) do
+         t = Turma.create!(
+          nome: "TURMA VELHA", 
+          codigo: "OLD0001",
+          docente: docente, semestre: "2024.1", horario: "35T23", status: true
+        )
+        t.materia = Materia.create!(nome: "Velha", codigo: "OLD0001")
+        t.save!
+        t
+      end
+
+      before do
+        allow(File).to receive(:read).with(satisfy { |p| p.to_s.include?('classes.json') }).and_return(classes_json)
+        allow(File).to receive(:read).with(satisfy { |p| p.to_s.include?('class_members.json') }).and_return(members_json)
+      end
+
+      it 'atualiza o e-mail e nome do aluno' do
+        described_class.call
+        aluno_existente.reload
+        
+        expect(aluno_existente.email).to eq("fulano.novo@gmail.com")
+        expect(aluno_existente.nome).to eq("Fulano da Silva")
+      end
+
+      it 'atualiza o nome da turma' do
+        described_class.call
+        turma_existente.reload
+        
+        expect(turma_existente.nome).to eq("BANCOS DE DADOS AVANÇADO")
+      end
+
+      it 'matricula o aluno na turma se ele não estava matriculado' do
+        expect(aluno_existente.turmas).to be_empty
+        
+        described_class.call
+        aluno_existente.reload
+        
+        expect(aluno_existente.turmas).to include(turma_existente)
+      end
+
+      it 'desativa alunos que não estão mais no arquivo' do
+        described_class.call
+        aluno_removido.reload
+        
+        expect(aluno_removido.status).to be(false)
+        expect(aluno_existente.reload.status).to be(true)
+      end
+
+      it 'desativa turmas que não estão mais no arquivo' do
+        described_class.call
+        turma_removida.reload
+        
+        expect(turma_removida.status).to be(false)
+        expect(turma_existente.reload.status).to be(true)
+      end
+    end
+
     context 'quando os arquivos não são encontrados' do
       before do
         allow(File).to receive(:read).and_raise(Errno::ENOENT)
