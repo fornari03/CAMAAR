42769e55981a2db0e9b0df3858b089aec8e349e2
fornari03 <221017023@aluno.unb.br>
2025-12-10 17:59:31 -0300

    feat: cria método de redefinição de senha no user mailer

--- src/app/mailers/user_mailer.rb
+++ src/app/mailers/user_mailer.rb
@@ -10,4 +10,14 @@ class UserMailer < ApplicationMailer
 
     mail(to: @user.email, subject: 'Definição de Senha - Sistema CAMAAR')
   end
+
+  def redefinicao_senha
+    @user = params[:user]
+    
+    token = @user.signed_id(purpose: :redefinir_senha, expires_in: 15.minutes)
+    
+    @url = edit_redefinir_senha_url(token: token)
+
+    mail(to: @user.email, subject: 'Redefinição de Senha - Sistema CAMAAR')
+  end
 end
