17ccf3629024b3a67f2fff6880e3fb1fa2b0d1c9
fornari03 <221017023@aluno.unb.br>
2025-12-14 16:38:27 -0300

    refac: show e generate_csv de ResultadosController

--- src/app/controllers/resultados_controller.rb
+++ src/app/controllers/resultados_controller.rb
@@ -2,56 +2,80 @@ require 'csv'
 
 class ResultadosController < ApplicationController
   before_action :authorize_admin
+  before_action :set_formulario, only: :show
+  before_action :load_respostas, only: :show
 
   def index
-    # List all forms for admin to see results status
     @formularios = Formulario.all.includes(:turma, :respostas)
   end
 
   def show
-    @formulario = Formulario.find(params[:id])
-    @respostas = @formulario.respostas.where.not(data_submissao: nil).includes(resposta_items: [:questao, :opcao_escolhida])
-
     respond_to do |format|
       format.html
-      format.csv do
-        if @respostas.empty?
-          redirect_to resultado_path(@formulario), alert: "Não é possível gerar um relatório, pois não há respostas."
-        else
-          filename = "relatorio_#{@formulario.titulo_envio.parameterize.underscore}.csv"
-          send_data generate_csv(@formulario, @respostas), filename: filename
-        end
-      end
+      format.csv { handle_csv_export }
     end
-  rescue ActiveRecord::RecordNotFound
-    redirect_to formularios_path, alert: "Formulário não encontrado"
   end
 
   private
 
   def authorize_admin
-    redirect_to root_path, alert: "Acesso restrito." unless current_usuario && current_usuario.admin?
+    redirect_to root_path, alert: "Acesso restrito." unless current_usuario&.admin?
   end
 
-  def generate_csv(formulario, respostas)
-    CSV.generate(headers: true) do |csv|
-      questions = formulario.template.questoes.order(:id)
-      
-      # Header
-      header = ["Timestamp", "Turma"] + questions.map(&:enunciado)
-      csv << header
+  def set_formulario
+    @formulario = Formulario.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to formularios_path, alert: "Formulário não encontrado"
+  end
 
-      # Rows
-      respostas.each do |resposta|
-        row = [resposta.data_submissao, formulario.turma.codigo]
-        
-        questions.each do |question|
-          item = resposta.resposta_items.find_by(questao: question)
-          row << (item ? (item.texto_resposta.presence || item.opcao_escolhida&.texto_opcao) : "")
-        end
-        
-        csv << row
+  def load_respostas
+    @respostas = @formulario.respostas
+                            .where.not(data_submissao: nil)
+                            .includes(resposta_items: %i[questao opcao_escolhida])
+  end
+
+  def handle_csv_export
+    if @respostas.empty?
+      redirect_to resultado_path(@formulario), alert: "Não é possível gerar um relatório, pois não há respostas."
+    else
+      send_data generate_csv_content, filename: csv_filename
+    end
+  end
+
+  def csv_filename
+    "relatorio_#{@formulario.titulo_envio.parameterize.underscore}.csv"
+  end
+
+  def generate_csv_content
+    questions = @formulario.template.questoes.order(:id)
+
+    CSV.generate(headers: true) do |csv|
+      csv << build_csv_header(questions)
+      
+      @respostas.each do |resposta|
+        csv << build_csv_row(resposta, questions)
       end
     end
+  end
+
+  def build_csv_header(questions)
+    ["Timestamp", "Turma"] + questions.map(&:enunciado)
+  end
+
+  def build_csv_row(resposta, questions)
+    base_data = [resposta.data_submissao, @formulario.turma.codigo]
+    
+    answers_data = questions.map do |question|
+      extract_answer_value(resposta, question)
+    end
+    
+    base_data + answers_data
+  end
+
+  def extract_answer_value(resposta, question)
+    item = resposta.resposta_items.find { |i| i.questao_id == question.id }
+    return "" unless item
+
+    item.texto_resposta.presence || item.opcao_escolhida&.texto_opcao
   end
 end
